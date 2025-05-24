efe7dd4d0f5e9ee8108a4e8d011f7abe7b06aaff
apolline <apolline.fontaine@epitech.eu>
2025-05-24 18:01:52 +0400

    ADD - setup sensor DHT11 and display information on an lcd screen

---
+++ nano1/nano1.ino
@@ -0,0 +1,52 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+// DHT11 setup
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  lcd.begin(16, 2);
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  delay(2000);
+}
+
+void loop() {
+    float temp = dht.readTemperature();
+    float hum = dht.readHumidity();
+
+    if (isnan(temp) || isnan(hum)) {
+      Serial.println("Erreur lecture DHT!");
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Erreur capteur");
+      delay(2000);
+      return;
+    }
+
+    lcd.clear();
+
+    lcd.setCursor(0, 0);
+    lcd.print("Temp: ");
+    lcd.print(temp);
+    lcd.setCursor(13, 0);
+    lcd.print((char)223); // symbole ° degré
+    lcd.print("C");
+
+    lcd.setCursor(0, 1);
+    lcd.print(" Hum: ");
+    lcd.print(hum);
+    lcd.setCursor(13, 1);
+    lcd.print(" %");
+
+    delay(2000); // Attente 2s avant prochaine mesure
+}
